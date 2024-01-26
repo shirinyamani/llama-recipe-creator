86e7066d8453b56c7826bc8078be92fa937b2512
shirinyamani <yamani.shirin@ucalgary.ca>
2024-01-25 17:07:52 -0700

    main added

---
+++ src/recipe.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+
+st.title("llama2 recipe generator")
---
+++ src/utils.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+
+
+def call_llama2(model, prompt, stream=False):
+    url = "http://localhost:11434/api/generate"
+    data = {
+        'model' : model,
+        'prompt' : prompt,
+        'stream' : stream
+    }
+    json_data = json.dumps(data)
+    response = requests.post(url, json_data, headers={'Content-Type': 'application/json'})
+    if response.status_code == 200:
+        return response.json()['response']
+    
+    return f'Error code is: {response.status_code}'
